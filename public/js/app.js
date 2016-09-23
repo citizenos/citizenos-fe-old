d9f79ac795bb07870ce2e860fcbafe7d32db590b
Lennart Rikk <lennart.rikk@gmail.com>
2016-09-23 21:29:57 +0300

    nav and content to responsive

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,46 @@
  *  @see https://angularjs.org/
  */
 
-var app = angular.module('citizenos', []);
+var app = angular.module('citizenos', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+
+	$locationProvider.html5Mode(true);
+
+	$routeProvider
+		.when('/', {
+			controller: 'home',
+			templateUrl: '../views/no_topics.html'
+		})
+
+	
+});
+
+/* Set global w_width variable for responsive dom hiding */
+app.directive('resize', function ($window, $rootScope) {
+    return function (scope, element) {
+		
+        var w = angular.element($window);
+        scope.getWindowDimensions = function () {
+            return {
+                'w': window.innerWidth
+            };
+        };
+		
+        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+			$rootScope.w_width = newValue.w;
+        }, true);
+
+        w.bind('resize', function () {
+            scope.$apply();
+        });
+    }
+});
 
 
+
+app.controller('home', ['$rootScope', function($rootScope) {
+
+
+
+}]);
