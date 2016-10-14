5a559e5ecefa8da62a4d53c0ad5178d3890111ff
Lennart Rikk <lennart.rikk@gmail.com>
2016-10-14 21:07:58 +0300

    dropdown logic development

--- public/js/app.js
+++ public/js/app.js
@@ -38,6 +38,29 @@ app.config(function ($routeProvider, $locationProvider) {
 
 });
 
+/* Close dropdowns when clicked outside of it */ 
+app.directive('dropdown', function($document) {
+	return {
+		restrict: "A",
+		link: function(scope, elem, attr) {
+			
+			elem.bind('click', function() {
+				elem.toggleClass('dropdown_active');
+				elem.addClass('active_recent');
+			});
+			
+			$document.bind('click', function() {
+				if(!elem.hasClass('active_recent')) {
+					elem.removeClass('dropdown_active');
+				}
+				elem.removeClass('active_recent');
+                this.unbind('click');
+			});
+			
+		}
+	}
+});
+
 /* Set global wWidth variable for responsive dom hiding */
 app.directive('resize', function ($window, $rootScope) {
     return function (scope, element) {
