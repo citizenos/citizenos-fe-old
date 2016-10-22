f66fcb872e5f4cd14079345dcc197bb2bb48e2a2
Mikk Andresen <mikk.andresen@gmail.com>
2016-10-22 15:02:30 +0300

    Home.html - working filters for desktop view.

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -3,9 +3,41 @@
 angular
     .module('citizenos')
     .controller('HomeCtrl', ['$scope', '$log', 'sTopic', function ($scope, $log, sTopic) {
-        $scope.isTopicListLoading = true;
+
+        // Constant marking the "clear" or all options will do
+        $scope.FILTERS_ALL = 'all';
+
+        $scope.filters = {
+            statuses: {
+                value: null,
+                options: [$scope.FILTERS_ALL].concat(_.values(sTopic.STATUSES))
+            },
+            categories: {
+                value: null,
+                options: [$scope.FILTERS_ALL].concat(_.values(sTopic.CATEGORIES))
+            },
+            limit: 50,
+            offset: 0,
+            infiniteScrollDisabled: true
+        };
+
+        /**
+         * Update topic list
+         *
+         * @param {object} filter The property of filter that changed. For ex filters.statuses, filters.categories
+         * @param {string} value The new value
+         */
+        $scope.doSetFilter = function (filter, value) {
+            if (value === $scope.FILTERS_ALL) {
+                value = null;
+            }
+            filter.value = value;
+
+            $scope.loadTopicList();
+        };
 
         $scope.topicList = [];
+        $scope.isTopicListLoading = true;
 
         // Running request, so I can cancel it if needed.
         var loadTopicListPromise = null;
@@ -17,18 +49,15 @@ angular
                 loadTopicListPromise.abort();
             }
 
-            loadTopicListPromise = sTopic.listUnauth();
+            loadTopicListPromise = sTopic.listUnauth($scope.filters.statuses.value, $scope.filters.categories.value, $scope.filters.offset, $scope.filters.limit);
 
             loadTopicListPromise
                 .then(function (res) {
-                        var topics = res.data.data.rows;
-                        if (topics && topics.length) {
-                            $scope.topicList = $scope.topicList.concat(topics);
-                        }
+                        $scope.topicList = res.data.data.rows;
                         $scope.isTopicListLoading = false;
                     },
-                    function (res) {
-                        $log.log('List fetch failed or was cancelled', res);
+                    function (err) {
+                        $log.log('List fetch failed or was cancelled', err);
                         $scope.isTopicListLoading = false;
                     });
         };
