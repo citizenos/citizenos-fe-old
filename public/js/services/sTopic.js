0a26d671575c432a413fe6a79dca1cef9ff958eb
ilmar <ilmar.tyrk@gmail.com>
2016-10-31 18:02:37 +0200

    SEO prerender and sTopic read single Topic to get SEO metainfo

--- public/js/services/sTopic.js
+++ public/js/services/sTopic.js
@@ -1,16 +1,16 @@
 angular
     .module('citizenos')
-    .service('sTopic', ['$http', '$log', 'sLocation', function ($http, $log, sLocation) {
-        var Topic = this;
+    .service('sTopic', ['$http', '$log', '$q', 'sLocation', function ($http, $log, $q, sLocation) {
+        var sTopic = this;
 
-        Topic.STATUSES = {
+        sTopic.STATUSES = {
             inProgress: 'inProgress', // Being worked on
             voting: 'voting', // Is being voted which means the Topic is locked and cannot be edited.
             followUp: 'followUp', // Done editing Topic and executing on the follow up plan.
             closed: 'closed' // Final status - Topic is completed and no editing/reopening/voting can occur.
         };
 
-        Topic.CATEGORIES = {
+        sTopic.CATEGORIES = {
             business: 'business', // Business and industry
             transport: 'transport', // Public transport and road safety
             taxes: 'taxes', // Taxes and budgeting
@@ -27,7 +27,7 @@ angular
             varia: 'varia' // Varia
         };
 
-        Topic.listUnauth = function (statuses, categories, offset, limit) {
+        sTopic.listUnauth = function (statuses, categories, offset, limit) {
             var path = sLocation.getAbsoluteUrlApi('/api/topics');
 
             return $http.get(path, {
@@ -38,5 +38,25 @@ angular
                     limit: limit
                 }
             });
+        };
+
+        sTopic.readUnauth = function (topic) {
+        //    $log.debug('readUnauth', topic);
+            var success = function (response) {
+                $log.debug('sTopic.SUCCESS', response);
+                return response.data.data;
+            };
+
+            var error = function (response) {
+                $log.debug('sTopic.ERROR', response);
+                return $q.reject(response);
+            };
+
+            var path = sLocation.getAbsoluteUrlApi('/api/topics/:topicId').replace(':topicId', topic.id);
+            return $http.get(path).then(success, error);
+        };
+
+        sTopic.create = function (topic) {
+            return $http.post(sLocation.getAbsoluteUrlApi('/api/users/self/topics'), topic);
         };
     }]);
