e7002968a7fa8962dc44e7e7daf9942d74c36b29
Mikk Andresen <mikk.andresen@gmail.com>
2016-11-21 18:43:46 +0200

    Login_esteid.html - DONE. Moved id_signing.html to modals/topic_vote_sign.html

--- public/js/controllers/AppCtrl.js
+++ public/js/controllers/AppCtrl.js
@@ -75,15 +75,6 @@ angular
             });
         };
 
-        $scope.app.doShowIdSigning = function () {
-            $log.debug('AppCtrl.doShowIdSigning()');
-
-            ngDialog.open({
-                template: '/views/lightboxes/id_signing.html',
-                scope: $scope
-            });
-        };
-
         $scope.app.doSwitchLanguage = function (language) {
             $log.debug('AppCtrl.doSwitchLanguage()', language);
             if (language === $scope.app.language) {
