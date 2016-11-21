94926f17ef621e2198be429f225d49c7c03ffbaf
Lennart Rikk <lennart.rikk@gmail.com>
2016-11-21 18:23:12 +0200

    login update

--- public/js/libs/template-cache.js
+++ public/js/libs/template-cache.js
@@ -17,12 +17,12 @@ angular
             '/views/default/nav_mobile.html',
             '/views/default/search.html',
             '/views/layouts/main.html',
-            '/views/lightboxes/id_signing.html',
             '/views/modals/login.html',
             '/views/modals/login_esteid.html',
             '/views/modals/password_forgot.html',
             '/views/modals/password_reset.html',
             '/views/modals/sign_up.html',
+            '/views/lightboxes/id_signing.html',
         ];
         var i = 0;
         if (templates.length) {
