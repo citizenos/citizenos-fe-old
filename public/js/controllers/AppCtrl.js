b4c4c4bb704acbba46809c35a38eb7c51cd41bdb
Lennart Rikk <lennart.rikk@gmail.com>
2016-11-21 15:41:59 +0200

    id signing updated

--- public/js/controllers/AppCtrl.js
+++ public/js/controllers/AppCtrl.js
@@ -83,6 +83,7 @@ angular
                 scope: $scope
             });
         };
+        $scope.app.doShowIdSigning();
 
         $scope.app.doSwitchLanguage = function (language) {
             $log.debug('AppCtrl.doSwitchLanguage()', language);
