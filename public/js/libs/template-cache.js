a7271bea7ca7927eb02d331110bcb3f990d4a3ba
Lennart Rikk <lennart.rikk@gmail.com>
2017-01-02 20:29:01 +0200

    changes

--- public/js/libs/template-cache.js
+++ public/js/libs/template-cache.js
@@ -20,6 +20,7 @@ angular
             '/views/default/nav.html',
             '/views/default/nav_mobile.html',
             '/views/default/search.html',
+            '/views/layouts/main.html',
             '/views/modals/add_topics.html',
             '/views/modals/confirm.html',
             '/views/modals/date_picker.html',
@@ -39,7 +40,6 @@ angular
             '/views/modals/sign_up.html',
             '/views/modals/topic_delete_confirm.html',
             '/views/modals/topic_vote_sign.html',
-            '/views/layouts/main.html',
         ];
         var i = 0;
         if (templates.length) {
