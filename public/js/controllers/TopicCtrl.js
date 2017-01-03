a4087a0dd8c65baccbb6471f8d80c1bc1c5f2d24
Mikk Andresen <mikk.andresen@gmail.com>
2017-01-03 19:14:30 +0200

    My_topics_topicId.html - Topic progress.

--- public/js/controllers/TopicCtrl.js
+++ public/js/controllers/TopicCtrl.js
@@ -2,7 +2,7 @@
 
 angular
     .module('citizenos')
-    .controller('TopicCtrl', ['$scope', '$state', '$stateParams', '$log', '$location', 'sTopic', 'sTranslate', function ($scope, $state, $stateParams, $log, $location, sTopic, sTranslate) {
+    .controller('TopicCtrl', ['$scope', '$state', '$stateParams', '$log', 'TopicMemberGroup', function ($scope, $state, $stateParams, $log, TopicMemberGroup) {
         $log.debug('TopicCtrl');
 
         $scope.topic = _.find($scope.itemList, {id: $stateParams.topicId});
@@ -10,5 +10,47 @@ angular
         $scope.generalInfo = {
             isVisible: true
         };
+
+        $scope.groupList = {
+            isVisible: false,
+            isSearchVisible: false,
+            searchFilter: '',
+            searchOrderBy: {
+                property: 'name'
+            }
+        };
+
+        $scope.doToggleGroupList = function (topic) {
+            if ($scope.groupList.isVisible) {
+                $scope.groupList.isVisible = false;
+                return;
+            }
+
+            TopicMemberGroup
+                .query({topicId: topic.id}).$promise
+                .then(function (groups) {
+                    topic.members.groups.rows = groups;
+                    topic.members.groups.count = groups.length;
+                    $scope.groupList.isVisible = true;
+                });
+        };
+
+        $scope.doUpdateMemberGroup = function (topic, topicMemberGroup, level) {
+            $log.debug('doUpdateMemberGroup', topic, topicMemberGroup, level);
+
+            if (topicMemberGroup.level !== level) {
+                var oldLevel = topicMemberGroup.level;
+                topicMemberGroup.level = level;
+                topicMemberGroup
+                    .$update({topicId: topic.id})
+                    .then(
+                        angular.noop,
+                        function () {
+                            topicMemberGroup.level = oldLevel;
+                        });
+            }
+        };
+
+        $scope.TopicMemberGroup = TopicMemberGroup;
 
     }]);
