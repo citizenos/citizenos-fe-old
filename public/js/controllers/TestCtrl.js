e12847e909a131965c42dc1890c0eb3c60bd1078
ilmar <ilmar.tyrk@gmail.com>
2017-02-01 17:57:41 +0200

    cosOverlay with input & datepicker directives, includes TestCtrl

---
+++ public/js/controllers/TestCtrl.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+    .module('citizenos')
+    .controller('TestCtrl', ['$scope', '$log', 'sTopic', function ($scope, $log, sTopic) {
+        $scope.item  = 10;
+        $scope.date  = new Date();
+        $scope.saveItem = function () {
+            console.log('saveItem');
+            console.log($scope.item);
+        }
+
+        $scope.saveDate = function () {
+            console.log('saveDate');
+            console.log($scope.date);
+        }
+
+    }]);
