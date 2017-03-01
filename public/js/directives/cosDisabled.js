a50334a78bf8df9e7e2495baba44e1bb28f48451
ilmar <ilmar.tyrk@gmail.com>
2017-03-01 17:56:33 +0200

    cosDisable update, disable all inner element click events

--- public/js/directives/cosDisabled.js
+++ public/js/directives/cosDisabled.js
@@ -1,9 +1,9 @@
 angular
     .module('citizenos')
-    .directive('cosDisabled', [function () {
+    .directive('cosDisabled', ['$timeout', function ($timeout) {
         return {
             restrict: 'A',
-            priority: -10,
+            priority : -10,
             link: function (scope, elem, attrs) {
                 var cosDisabled = scope.$eval(attrs.cosDisabled);
                 var classDisabled = 'disabled';
@@ -30,7 +30,16 @@ angular
                     }
                 });
 
+                $timeout(function () { //To disable all innerElement click events $timeout helps to override ng-click events created inside other directives eg. cosToggle
+                    elem.find('*').on('click', function (e) {
+                        if(cosDisabled) {
+                            e.stopImmediatePropagation();
+                        }
+                    });
+                });
+
                 scope.$watch(function () {
+                    cosDisabled = scope.$eval(attrs.cosDisabled);
                     return cosDisabled;
                 }, function () {
                     update();
@@ -39,4 +48,3 @@ angular
             }
         }
     }]);
-
