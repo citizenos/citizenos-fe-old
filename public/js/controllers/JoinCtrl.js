0df5a4250fed96d5596f12a92f8da370aed647d2
Mikk Andresen <mikk.andresen@gmail.com>
2017-03-21 18:27:08 +0200

    Partner auth flow. Dynamic logo by Partner ID. WIP

--- public/js/controllers/JoinCtrl.js
+++ public/js/controllers/JoinCtrl.js
@@ -1,26 +1,28 @@
 'use strict';
 
-app.controller('JoinCtrl', ['$scope', '$rootScope', '$state', '$stateParams', '$location', '$filter', '$log', 'Topic', function ($scope, $rootScope, $state, $stateParams, $location, $filter, $log, Topic) {
-    var joinTopic = new Topic();
-    joinTopic
-        .$join({tokenJoin:$stateParams.tokenJoin})
-        .then(function (res) {
+angular
+    .module('citizenos')
+    .controller('JoinCtrl', ['$scope', '$rootScope', '$state', '$stateParams', '$location', '$filter', '$log', 'Topic', function ($scope, $rootScope, $state, $stateParams, $location, $filter, $log, Topic) {
+        var joinTopic = new Topic();
+        joinTopic
+            .$join({tokenJoin: $stateParams.tokenJoin})
+            .then(function (res) {
                 $state.go('my.topics.topicId', {
                     topicId: res.data.id
                 });
-        }, function (res) {
-            $log.error('Failed to join Topic ', res);
-            var status = res.data.status;
-            if (status.code === 40100) { // Unauthorized
-                // Redirect to login.
-                $log.error('Failed to join', status.code, status.message);
-                var currentUrl = $state.href($state.current.name, $stateParams);
-                $state.go('account.login', {redirectSuccess: currentUrl});
-            } else if (status.code === 40001) { // Matching token not found.
-                $state
-                    .go('home');
-            } else {
-                $log.error('Failed to join Topic', res);
-            }
-        });
-}]);
+            }, function (res) {
+                $log.error('Failed to join Topic ', res);
+                var status = res.data.status;
+                if (status.code === 40100) { // Unauthorized
+                    // Redirect to login.
+                    $log.error('Failed to join', status.code, status.message);
+                    var currentUrl = $state.href($state.current.name, $stateParams);
+                    $state.go('account.login', {redirectSuccess: currentUrl});
+                } else if (status.code === 40001) { // Matching token not found.
+                    $state
+                        .go('home');
+                } else {
+                    $log.error('Failed to join Topic', res);
+                }
+            });
+    }]);
