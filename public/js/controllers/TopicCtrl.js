49de1b139c4a8ae55afa90d20e262effd54db03c
Mikk Andresen <mikk.andresen@gmail.com>
2017-03-22 16:43:31 +0200

    Translations - login.html

--- public/js/controllers/TopicCtrl.js
+++ public/js/controllers/TopicCtrl.js
@@ -127,7 +127,7 @@ angular
                     }
                 }, angular.noop);
             }
-        }
+        };
 
         $scope.loadTopicSocialMentions = function () {
             if ($scope.topic.hashtag) {
