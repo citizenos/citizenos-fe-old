95a0d8fa0f9d0bc86e35db1097561d08d457b635
Mikk Andresen <mikk.andresen@gmail.com>
2017-04-04 23:56:16 +0300

    Put back the tutorial.

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -60,8 +60,7 @@ angular
         $scope.isTutorialVisible = function () {
             return $scope.filters.categories.value === $scope.FILTERS_ALL
                 && $scope.filters.statuses.value === $scope.FILTERS_ALL
-                && $scope.topicList.length // Render tutorial only when there are topics, this avoids Android and alignment issues.
-                && false; // Disable tutorial while we have nothing to back it.
+                && $scope.topicList.length; // Render tutorial only when there are topics, this avoids Android and alignment issues.
         };
 
         $scope.loadTopicList = function () {
