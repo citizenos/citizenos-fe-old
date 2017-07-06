3c9e1d52754c790405da0785f4b675a04a31bfcf
ilmar <ilmar.tyrk@gmail.com>
2017-07-06 17:56:26 +0300

    topic attachment file upload, TopicAttachment service

--- public/js/controllers/forms/MyAccountFormCtrl.js
+++ public/js/controllers/forms/MyAccountFormCtrl.js
@@ -2,7 +2,7 @@
 
 angular
     .module('citizenos')
-    .controller('MyAccountFormCtrl', ['$scope', '$log', '$stateParams', '$filter', 'ngDialog', 'sAuth', 'sUser', function ($scope, $log, $stateParams, $filter, ngDialog, sAuth, sUser) {
+    .controller('MyAccountFormCtrl', ['$scope', '$log', '$stateParams', '$filter', '$document', 'ngDialog', 'sAuth', 'sUser', 'sUpload', function ($scope, $log, $stateParams, $filter, $document, ngDialog, sAuth, sUser, sUpload) {
         $log.debug('MyAccountFormCtrl');
 
         $scope.form = {
@@ -12,6 +12,8 @@ angular
             company: null,
             imageUrl: null
         };
+
+        $scope.imageFile = null;
         angular.extend($scope.form, sAuth.user);
 
         $scope.doUpdateProfile = function () {
@@ -26,9 +28,48 @@ angular
                 $scope.errors = res.data.errors;
             };
 
-            sUser
-                .update($scope.form.name, $scope.form.email, $scope.form.password, $scope.form.company)
-                .then(success, error);
+            if($scope.imageFile) {
+                sUpload
+                    .upload($scope.imageFile, 'users')
+                    .then(function (url) {
+                            $scope.form.imageUrl = url;
+                            sUser
+                                .update($scope.form.name, $scope.form.email, $scope.form.password, $scope.form.company, url)
+                                .then(success, error);
+                        });
+
+            } else {
+                sUser
+                    .update($scope.form.name, $scope.form.email, $scope.form.password, $scope.form.company, $scope.form.imageUrl)
+                    .then(success, error);
+            }
+        };
+
+        $scope.uploadImage = function () {
+            $document[0].getElementById('profileImage').click();
+        };
+
+        $scope.switchImage = function (element) {
+            $scope.imageFile = element.files[0];
+            var reader = new FileReader();
+            reader.onload = (function() { return function(e) {
+                $scope.$apply(function () {
+                    $scope.form.imageUrl = e.target.result;
+                });
+            };
+            })();
+            reader.readAsDataURL(element.files[0]);
+        };
+
+        $scope.deleteProfileImage = function () {
+            if($scope.form.imageUrl.indexOf('amazonaws') > -1) {
+                sUpload.delete($scope.form.imageUrl, 'users')
+                    .then(function () {
+                        sUser
+                            .update($scope.form.name, $scope.form.email, $scope.form.password, $scope.form.company, '')
+                            .then(function (res) { angular.extend(sAuth.user, res.data.data);}, function (res) { $scope.errors = res.data.errors;})
+                    });
+            }
         };
 
     }]);
