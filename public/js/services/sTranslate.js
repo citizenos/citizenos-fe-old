5f397f6e2c0f5ea75766434a2630a16d889a152c
ilmar <ilmar.tyrk@gmail.com>
2018-01-05 01:17:48 +0200

    language switcher fix

--- public/js/services/sTranslate.js
+++ public/js/services/sTranslate.js
@@ -32,24 +32,26 @@ angular
         };
 
         sTranslate.switchLanguage = function (language) {
-            $log.debug('switch language', language);
-            if (sTranslate.checkLanguageIsValid(language)) {
-                $stateParams.language = language;
-                if (language === 'aa') { // Crowdin language selected, we need a full page reload for the in-context script to work.
-                    window.location.href = $state.href($state.current.name, $stateParams);
-                } else {
-                    $state.transitionTo($state.current.name, $stateParams);
+            $translate.onReady(function () {
+                $log.debug('switch language', language);
+                if (sTranslate.checkLanguageIsValid(language)) {
+                    $stateParams.language = language;
+                    if (language === 'aa') { // Crowdin language selected, we need a full page reload for the in-context script to work.
+                        window.location.href = $state.href($state.current.name, $stateParams);
+                    } else {
+                        $state.transitionTo($state.current.name, $stateParams);
+                    }
                 }
-            }
-            sTranslate.setLanguage(language);
+                sTranslate.setLanguage(language);
+            });
         };
 
         sTranslate.debugMode = function () {
             $translate.onReady(function () {
-                if ($translate.use() !== debugLang) {
-                    $translate.use(debugLang);
-                } else {
+                if ($translate.use() === debugLang) {
                     $translate.use(sTranslate.currentLanguage);
+                } else {
+                    $translate.use(debugLang);
                 }
             });
         };
