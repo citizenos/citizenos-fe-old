003644b24b73b9bff8141bd1475ddec0e370db0c
Mikk Andresen <mikk.andresen@gmail.com>
2018-04-25 14:56:42 +0300

    Revert "BUG: Home - fix wrapping of topic title and description. Bug was due to the fact that EP adds &nbsp; here and there thus making it unwrappable in the FE."
    This reverts commit 0c3aba2b4a870e7b5e849b211ea6f5681d3161f9.

--- public/js/filters/cosRemoveNbsp.js
+++
@@ -1,7 +0,0 @@
-angular
-    .module('citizenos')
-    .filter('cosRemoveNbsp', function () {
-        return function cosRemoveWhitespace(text) {
-            return text.replace(/&nbsp;/g, ' ');
-        }
-    });
