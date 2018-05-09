fdbc0cc449f8a0177d6c90d3ee8d4aa17ade8e9a
Mikk Andresen <mikk.andresen@gmail.com>
2018-05-09 18:16:11 +0300

    cosTouchSlider - simplify code a bit, remove hacks.

--- public/js/directives/cosTouchSlider.js
+++ public/js/directives/cosTouchSlider.js
@@ -1,8 +1,7 @@
 angular
     .module('citizenos')
     .directive('slideController', ['$timeout', '$swipe', '$rootScope', '$window', '$parse',
-        function ($timeout, $swipe, $rootScope, $window, $parse) {
-
+        function ($timeout, $swipe, $rootScope, $window) {
             return {
                 restrict: 'EA',
                 /* Do one-way binding for attribute start_slide_at, NOTE camelcase */
@@ -12,73 +11,49 @@ angular
                     class: '@'
                 },
                 link: function (scope, element, attrs) {
-                    if (scope.startSlideAt >= $rootScope.wWidth) {
+                    var getElementStyle = function (element) {
+                        return $window.getComputedStyle(element[0], null);
+                    };
+
+                    var getElementWidth = function (element) {
+                        return Number($window.getComputedStyle(element[0], null).width.replace('px', ''));
+                    };
+
+                    var getElementMarginLeft = function (element) {
+                        return Number($window.getComputedStyle(element[0], null)['margin-left'].replace('px', ''));
+                    };
+
+                    var getElementPaddingLeft = function (element) {
+                        return Number($window.getComputedStyle(element[0], null)['padding-left'].replace('px', ''));
+                    };
+
+                    var getElementMaxDistanceLeft = function (element) {
+                        return (getElementMarginLeft(element) + getElementWidth(element)) * -1 + $rootScope.wWidth - getElementPaddingLeft(element);
+                    };
+
+                    element.ready(function () {
                         /* Define multiple variables */
-                        var startX, elementX_start, elementX_live, element_max_distance_left, target_position, last_element_position, element_drag_speed, stop_position, ease_animation, new_element_position, neg_ili_pos, moved_amount, element_style;
+                        var startX, target_position, last_element_position, element_drag_speed, stop_position, ease_animation, new_element_position;
+                        var element_style = getElementStyle(element);
+                        var element_width = getElementWidth(element);
+                        var element_max_distance_left = getElementMaxDistanceLeft(element);
+
                         var speeding_ticket;
-
-                        /* Guarantee that on window resize the position of the slidable object won't break */
-                        var moved_percent = scope.identifier + '.moved_percent';
-
-                        scope.getWindowDimensions = function () {
-                            return {
-                                'w': window.innerWidth
-                            };
-                        };
-
-                        element_style = $window.getComputedStyle(element[0], null);
-
-                        /* This function needs to run on start up and on resize of the window */
-                        function run_me_twice() {
-
-                            /* Use timeout so other directives can run their course */
-                            $timeout(function () {
-                                element_max_distance_left = $rootScope.wWidth - Number(element_style.getPropertyValue('width').replace('px', '')) - 20;
-
-                                element[0].style.marginLeft = element_max_distance_left * ($rootScope.$eval(moved_percent) / 100) + 'px';
-
-                                if (element_max_distance_left > 0) {
-                                    element[0].style.marginLeft = 0;
-                                }
-                            }, 0);
-
-                        }
-
-
-                        scope.check_width = setInterval(function () {
-                            if (element_style.getPropertyValue('width') != '100%' && element_style.getPropertyValue('width') != '0px') {
-                                run_me_twice();
-                                clearInterval(scope.check_width);
-                            }
-                        }, 50);
-
-
-                        /* On window resize reposition elements */
-                        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
-                            if (newValue != oldValue) {
-                                run_me_twice();
-                            }
-                        }, true);
-
 
                         /* Start touch-drag code */
                         $swipe.bind(element, {
                             'start': function (coords) {
                                 /* Get mouse starting coordinates */
                                 startX = coords.x;
-                                /* Get target element starting position */
-                                elementX_start = Number(element_style.getPropertyValue('margin-left').replace('px', ''));
-
 
                                 /* Clear interval that was set on drag end */
                                 clearInterval(ease_animation);
 
-                                last_element_position = element_style.getPropertyValue('margin-left').replace('px', '');
-
+                                last_element_position = element_style['margin-left'].replace('px', '');
 
                                 /* Get current speed of draggable object */
                                 speeding_ticket = setInterval(function () {
-                                    new_element_position = element_style.getPropertyValue('margin-left').replace('px', '');
+                                    new_element_position = element_style['margin-left'].replace('px', '');
                                     element_drag_speed = Math.abs(last_element_position - new_element_position);
 
                                     /* Limit maximum drag speed */
@@ -92,34 +67,30 @@ angular
                                     }
 
                                     /* Update kast element position to use it in new interval */
-                                    last_element_position = element_style.getPropertyValue('margin-left').replace('px', '');
+                                    last_element_position = element_style['margin-left'].replace('px', '');
 
                                 }, 20);
 
 
                             },
                             'move': function (coords) {
-
-                                /* Only do start draggin motion if window width is at lower than defined variable in DOM */
-
-                                if (scope.startSlideAt >= $rootScope.wWidth) {
+                                if (element_width >= $rootScope.wWidth) {
                                     /* Get delta by subtracting mouse drag start coordinates by current live mouse coordinates */
                                     var delta = coords.x - startX;
 
                                     /* Set element position vairable */
-                                    elementX_live = elementX_start + delta;
+                                    var element_position_current = getElementMarginLeft(element) + delta;
                                     /* Make sure div stays inside boundaries and if it's outside, set correct position */
-                                    if (elementX_live > element_max_distance_left && elementX_live < 0) {
-                                        target_position = elementX_live;
-                                    } else if (elementX_live < element_max_distance_left && elementX_live < 0) {
+                                    if (element_position_current > element_max_distance_left && element_position_current < 0) {
+                                        target_position = element_position_current;
+                                    } else if (element_position_current < element_max_distance_left && element_position_current < 0) {
                                         target_position = element_max_distance_left;
-                                    } else if (elementX_live > 0) {
+                                    } else if (element_position_current > 0) {
                                         target_position = 0;
                                     }
 
                                     /* Set element position live */
                                     element[0].style.marginLeft = target_position + 'px';
-
                                 }
                             },
                             'end': function (coords) {
@@ -127,7 +98,7 @@ angular
                                 /* Clear interval that was set on drag start */
                                 clearInterval(speeding_ticket);
                                 /* Get current position of the element */
-                                var elementX_current_position = Number(element_style.getPropertyValue('margin-left').replace('px', ''));
+                                var elementX_current_position = Number(element_style['margin-left'].replace('px', ''));
 
                                 /* Ease element to final position */
                                 /* Only do the ease if easing speed is greater than 0 and the element is inside the boundaries and not at the sides of the boundaries */
@@ -138,7 +109,7 @@ angular
 
                                         /* Set deceleration speed */
                                         element_drag_speed = element_drag_speed / 1.1;
-                                        elementX_current_position = Number(element_style.getPropertyValue('margin-left').replace('px', '')) + element_drag_speed;
+                                        elementX_current_position = Number(element_style['margin-left'].replace('px', '')) + element_drag_speed;
 
                                         /* If element's speed is lower than 0.1 stop the interval to avoid unneeded calculations */
                                         if (Math.abs(element_drag_speed) < 0.1) {
@@ -158,19 +129,24 @@ angular
                                         element[0].style.marginLeft = stop_position + 'px';
                                     }, 10);
                                 }
-
-                                /* Get the final position of the element */
-                                var elementX_end = Number(element_style.getPropertyValue('margin-left').replace('px', ''));
-
-                                /* Set the percentage variable to be used in case of window resize, set it to rootScope so it can be used again when leaving the page */
-                                moved_amount = (((element_max_distance_left - elementX_end)) / element_max_distance_left) * -100 + 100;
-                                $parse(moved_percent).assign($rootScope, moved_amount);
                             },
                             'cancel': function (coords) {
                                 clearInterval(speeding_ticket);
                             }
                         });
-                    }
+
+                        var onWindowResize = function () {
+                            element_max_distance_left = getElementMaxDistanceLeft(element);
+                        };
+
+                        var window = angular.element($window);
+
+                        window.bind('resize', onWindowResize);
+
+                        scope.$on('$destroy', function () {
+                            window.unbind('resize', onWindowResize);
+                        });
+                    });
                 }
             }
         }]);
@@ -181,9 +157,8 @@ angular
     .module('citizenos')
     .directive('dimensions', ['$timeout', '$rootScope',
         function ($timeout, $rootScope) {
-
             //Set initial tab lengh variable
-            $rootScope.tabLength = 0;
+            $rootScope.tabLength = 0; // Ugh, not nice to use $rootScope like this...
             return {
 
                 link: function (scope, elem) {
@@ -272,7 +247,7 @@ angular
             scope: true,
             link: function (scope, el, attrs) {
                 $timeout(init, false);
-                function init() {
+                function init () {
                     scope.offsetHeight = el[0].offsetHeight;
                     scope.readMore = false;
                     scope.maxTextHeight = 200;
