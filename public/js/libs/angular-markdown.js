f8f5780e2b876e9cff205dd99e26e5756f5193b5
ilmar <ilmar.tyrk@gmail.com>
2021-01-13 14:24:45 +0200

    Add character limit and counter

--- public/js/libs/angular-markdown.js
+++ public/js/libs/angular-markdown.js
@@ -33,27 +33,63 @@ angular.module('cosmarkdown', ['ngSanitize']).
       return $sce.getTrustedHtml(div.textContent);
     }
   }]).
-  directive('cosMarkdown', ['$sanitize', 'markdown', function ($sanitize, markdown) {
+  directive('cosMarkdown', ['$sanitize', '$filter', 'markdown', function ($sanitize, $filter, markdown) {
     return {
       restrict: 'A',
       scope: {
-        item: '=ngModel'
+        item: '=ngModel',
+        limit: '=maxlength'
       },
       link: function (scope, element, attrs) {
+        var curLength = 0;
+        var getCharLength = function () {
+          if (scope.item && scope.item.length) {
+            curLength = scope.item.length;
+          }
+
+          return curLength;
+        }
         var config = {
           placeholder: attrs.placeholder,
           toolbar: ["bold", "italic", "strikethrough", "|", "ordered-list", "unordered-list", "preview"],
           preview:true,
-          styleSelectedText: false,
           blockStyles: {
             italic: "_"
           },
+          status: [{
+            className: "charCounter",
+            defaultValue: function(el) {
+              el.innerHTML = scope.limit - getCharLength();
+            },
+            onUpdate: function(el) {
+              el.innerHTML = scope.limit - getCharLength();
+            }
+          }],
           element:element[0],
-          initialValue: scope.item
+          initialValue: scope.item,
+          toolbarTips: false
         };
 
         var simplemde = new markdown(config);
+        var enforceMaxLength = function(cm, change) {
+          var maxLength = cm.getOption("maxLength");
+          if (maxLength && change.update) {
+              var str = change.text.join("\n");
+              var delta = str.length-(cm.indexFromPos(change.to) - cm.indexFromPos(change.from));
+              if (delta <= 0) { return true; }
+              delta = cm.getValue().length+delta-maxLength;
+              if (delta > 0) {
+                  str = str.substr(0, str.length-delta);
+                  change.update(change.from, change.to, str.split("\n"));
+              }
+          }
+          return true;
+      }
+
+      simplemde.codemirror.setOption("maxLength", scope.limit);
+      simplemde.codemirror.on("beforeChange", enforceMaxLength);
         simplemde.codemirror.on("change", function(){
+          curLength = simplemde.value().length;
           scope.$apply(function () {
             scope.item = simplemde.value();
           });
