60701677b776c020e2c782abf584f7252b4e25c1
ilmar <ilmar.tyrk@gmail.com>
2021-11-05 09:34:53 +0200

    initial changes

--- public/js/app.js
+++ public/js/app.js
@@ -489,6 +489,23 @@
                         });
                     }]
                 })
+                .state('topics.view.participants', {
+                    url: '/invite?tab',
+                    parent: 'topics.view',
+                    reloadOnSearch: false,
+                    controller: ['$scope', '$state', '$stateParams', 'ngDialog', function ($scope, $state, $stateParams, ngDialog) {
+                        var dialog = ngDialog.open({
+                            template: '/views/modals/topic_members.html',
+                            data: $stateParams,
+                            scope: $scope // Pass on $scope so that I can access AppCtrl
+                        });
+                        dialog.closePromise.then(function (data) {
+                            if (data.value !== '$navigation') { // Avoid running state change when ngDialog is already closed by a state change
+                                $state.go('^');
+                            }
+                        });
+                    }]
+                })
                 .state('topics.view.files', {
                     url: '/files',
                     parent: 'topics.view',
