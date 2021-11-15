155e72977e603ab0dd307fad36262337c50ca03c
ilmar <ilmar.tyrk@gmail.com>
2021-11-15 22:51:56 +0200

    Finalize topic members modal

--- public/js/controllers/forms/TopicMembersCtrl.js
+++ public/js/controllers/forms/TopicMembersCtrl.js
@@ -16,29 +16,29 @@ angular
         var ITEMS_COUNT_PER_PAGE = 2;
 
         $scope.groupList = {
-            isVisible: false,
             isSearchVisible: false,
             searchFilter: '',
             searchOrderBy: {
-                property: 'name'
+                property: 'name',
+                sortOrder: 'ASC'
             }
         };
 
         $scope.userList = {
-            isVisible: false,
             isSearchVisible: false,
             searchFilter: '',
             searchOrderBy: {
-                property: 'name'
+                property: 'name',
+                sortOrder: 'ASC'
             }
         };
 
         $scope.inviteList = {
-            isVisible: false,
             isSearchVisible: false,
             searchFilter: '',
             searchOrderBy: {
-                property: 'name'
+                property: 'name',
+                sortOrder: 'ASC'
             }
         };
 
@@ -47,12 +47,18 @@ angular
             $location.search({tab: tab});
         };
 
-        $scope.loadMemberUserList = function (offset, limit) {
+        $scope.loadMemberUserList = function (offset, limit, order, sortOrder) {
             if (!limit) {
                 limit = ITEMS_COUNT_PER_PAGE;
             }
             if (!offset) {
                 offset = 0;
+            }
+            if (order) {
+                $scope.userList.searchOrderBy.property = order;
+            }
+            if (sortOrder) {
+                $scope.userList.searchOrderBy.sortOrder = sortOrder;
             }
 
             var search = null;
@@ -65,7 +71,9 @@ angular
                     topicId: $scope.topic.id,
                     offset: offset,
                     search: search,
-                    limit: limit
+                    limit: limit,
+                    order: $scope.userList.searchOrderBy.property,
+                    sortOrder: $scope.userList.searchOrderBy.sortOrder
                 }).$promise
                 .then(function (users) {
                     $scope.topic.members.users.rows = users;
@@ -85,7 +93,33 @@ angular
             $scope.loadMemberUserList(offset, ITEMS_COUNT_PER_PAGE);
         };
 
-        $scope.loadInviteUserList = function (offset, limit) {
+        $scope.sortParticipants  = function (property) {
+            if (property === $scope.userList.searchOrderBy.property && $scope.userList.searchOrderBy.sortOrder === 'ASC') {
+               order = 'DESC'
+            } else {
+                order = 'ASC'
+            }
+
+            $scope.loadMemberUserList(0, ITEMS_COUNT_PER_PAGE, property, order);
+        }
+
+        $scope.doUpdateMemberUser = function (topicMemberUser, level) {
+            if (topicMemberUser.level !== level) {
+                var oldLevel = topicMemberUser.level;
+                topicMemberUser.level = level;
+                topicMemberUser
+                    .$update({topicId: $scope.topic.id})
+                    .then(
+                        function () {
+                            topicMemberUser.levelUser = level;
+                        },
+                        function (res) {
+                            topicMemberUser.level = oldLevel;
+                        });
+            }
+        };
+
+        $scope.loadInviteUserList = function (offset, limit, order, sortOrder) {
             if (!limit) {
                 limit = ITEMS_COUNT_PER_PAGE;
             }
@@ -98,15 +132,23 @@ angular
                 search = $scope.inviteList.searchFilter.trim();
             }
 
+            if (order) {
+                $scope.inviteList.searchOrderBy.property = order;
+            }
+            if (sortOrder) {
+                $scope.inviteList.searchOrderBy.sortOrder = sortOrder;
+            }
+
             return TopicInviteUser
                 .query({
                     topicId: $scope.topic.id,
                     offset: offset,
                     search: search,
-                    limit: limit
+                    limit: limit,
+                    order: $scope.inviteList.searchOrderBy.property,
+                    sortOrder: $scope.inviteList.searchOrderBy.sortOrder
                 }).$promise
                 .then(function (invites) {
-                    console.log(invites)
                     $scope.topic.members.invited = {
                         rows: [],
                         count: 0
@@ -122,7 +164,6 @@ angular
 
                     $scope.topic.members.invited.rows = _.sortedUniqBy(inviteListOrderedByLevel, 'user.id');
                     $scope.topic.members.invited.count = invites.length;
-                    console.log($scope.topic.members);
                     if (invites.length) {
                         $scope.topic.members.invited.count = invites[0].countTotal;
                     }
@@ -130,7 +171,6 @@ angular
                     $scope.topic.members.invited.totalPages = Math.ceil($scope.topic.members.invited.count / limit);
                     $scope.topic.members.invited.page = Math.ceil((offset + limit) / limit);
 
-                    console.log($scope.topic.members);
                     return invites;
                 });
         };
@@ -140,7 +180,36 @@ angular
             $scope.loadInviteUserList(offset, ITEMS_COUNT_PER_PAGE);
         };
 
-        $scope.loadMemberGroupList = function (offset, limit) {
+        $scope.sortInvites = function (property) {
+            if (property === $scope.inviteList.searchOrderBy.property && $scope.inviteList.searchOrderBy.sortOrder === 'ASC') {
+               order = 'DESC'
+            } else {
+                order = 'ASC'
+            }
+
+            $scope.loadInviteUserList(0, ITEMS_COUNT_PER_PAGE, property, order);
+        }
+
+        $scope.doUpdateInvite = function (topicInviteUser, level) {
+            $log.debug('doUpdateMemberGroup', topicInviteUser, level);
+
+            if (topicInviteUser.level !== level) {
+                var oldLevel = topicInviteUser.level;
+                topicInviteUser.level = level;
+                topicInviteUser
+                    .$update({topicId: $scope.topic.id})
+                    .then(
+                        function () {
+                            topicInviteUser.level = level;
+                        },
+                        function () {
+                            topicInviteUser.level = oldLevel;
+                        }
+                    );
+            }
+        };
+
+        $scope.loadMemberGroupList = function (offset, limit, order, sortOrder) {
             if (!limit) {
                 limit = ITEMS_COUNT_PER_PAGE;
             }
@@ -152,10 +221,19 @@ angular
                 search = $scope.groupList.searchFilter.trim();
             }
 
+            if (order) {
+                $scope.groupList.searchOrderBy.property = order;
+            }
+            if (sortOrder) {
+                $scope.groupList.searchOrderBy.sortOrder = sortOrder;
+            }
+
             return TopicMemberGroup
                 .query({
                     topicId: $scope.topic.id,
                     offset: offset,
+                    order: $scope.groupList.searchOrderBy.property,
+                    sortOrder: $scope.groupList.searchOrderBy.sortOrder,
                     search: search,
                     limit: limit
                 }).$promise
@@ -175,6 +253,37 @@ angular
         $scope.loadMemberGroupPage = function (page) {
             var offset = (page - 1) * ITEMS_COUNT_PER_PAGE;
             $scope.loadMemberGroupList(offset, ITEMS_COUNT_PER_PAGE);
+        };
+
+        $scope.sortGroups  = function (property) {
+            if (property === $scope.groupList.searchOrderBy.property && $scope.groupList.searchOrderBy.sortOrder === 'ASC') {
+               order = 'DESC'
+            } else {
+                order = 'ASC'
+            }
+
+            $scope.loadMemberGroupList(0, ITEMS_COUNT_PER_PAGE, property, order);
+        }
+
+        $scope.doUpdateMemberGroup = function (topicMemberGroup, level) {
+            $log.debug('doUpdateMemberGroup', topicMemberGroup, level);
+
+            if (topicMemberGroup.level !== level) {
+                var oldLevel = topicMemberGroup.level;
+                topicMemberGroup.level = level;
+                topicMemberGroup
+                    .$update({topicId: $scope.topic.id})
+                    .then(
+                        function () {
+                            if ($scope.userList.isVisible) { // Reload User list when Group permissions change as User permissions may also change
+                                $scope.loadTopicMemberUserList();
+                            }
+                        },
+                        function () {
+                            topicMemberGroup.level = oldLevel;
+                        }
+                    );
+            }
         };
 
         $scope.loadMemberUsersPage();
