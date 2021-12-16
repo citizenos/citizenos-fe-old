903e7feeee1d8c7c0980b97167c38d3a550b69e8
ilmar <ilmar.tyrk@gmail.com>
2021-12-16 08:42:37 +0200

    Add ENV partner style option

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 'use strict';
 
-var config = require('config');
-var express = require('express');
-var app = express();
-var https = require('https');
-var path = require('path');
-var http = require('http');
-var fs = require('fs');
-var _ = require('lodash');
+const config = require('config');
+const express = require('express');
+const app = express();
+const https = require('https');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
 const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
 
-var prerender = require('prerender-node');
+const prerender = require('prerender-node');
 
-var configFe = config.util.loadFileConfigs('./public/config');
+const configFe = config.util.loadFileConfigs('./public/config');
 
-var pathSettings = path.resolve('./public/settings.js');
+const pathSettings = path.resolve('./public/settings.js');
 try {
-    var settingsFileTxt = '(function (window) { window.__config = window.__config || {};';
+    let settingsFileTxt = '(function (window) { window.__config = window.__config || {};';
     _(configFe).forEach(function (value, key) {
         settingsFileTxt += ' window.__config.' + key + ' = ' + JSON.stringify(value) + ';';
     });
@@ -27,9 +27,48 @@ try {
     console.log('Settings.json write FAILED to ' + pathSettings, err);
     process.exit(1);
 }
+// CRETE CSS FROM ENV
+
+if (config.customStyle) {
+    let css = '';
+    if (config.customStyle['logo-src']) {
+        const parsed = new URL(config.customStyle['logo-src']);
+        const ext = path.extname(parsed.pathname);
+        const filepath = './public/config/styles/logo'+ext;
+        const file = fs.createWriteStream(filepath);
+        new Promise(function (resolve, reject) {
+            https.get(parsed.href, function (response) {
+                response.pipe(file);
+                file.on('finish', function () {
+                    file.close();
+                    css += ' .logo_area .logo {background-image: url(\'/config/styles/logo'+ext+'\')}';
+                    return resolve();
+                });
+            }).on('error', function (err) { // Handle errors
+                fs.unlink(filepath, function ()  {
+                    return reject(err);
+                });
+            });
+        }).then(function ()  {
+            if (config.customStyle.css) {
+                css+= config.customStyle.css
+            }
+            fs.writeFileSync('./public/config/styles/local.css', css);
+        }, function (err) {
+            console.log('Logo file download failed: ', err)
+        });
+
+    } else if (config.customStyle.css) {
+        css+= config.customStyle.css
+    }
+    fs.writeFileSync('./public/config/styles/local.css', css);
+
+}
+
+
 //TODO: list whitelisted urls to github with description
-var cspConfig = config.csp;
-var cspOptions = _.cloneDeep(cspConfig);
+const cspConfig = config.csp;
+const cspOptions = _.cloneDeep(cspConfig);
 if (cspConfig) {
     if (cspConfig.directives) {
         if (typeof cspConfig.directives === 'string') {
@@ -68,7 +107,7 @@ const browserDetect = (req, res, next) => {
     const msie = ua.indexOf('MSIE ');
     if (msie > 0) {
         // IE 10 or older => return version number
-        var IEversion =  parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        const IEversion =  parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
         console.log('IEversion', IEversion);
         res.sendFile(__dirname + '/public/views/unknown_device.html');
 
@@ -78,9 +117,9 @@ const browserDetect = (req, res, next) => {
     const trident = ua.indexOf('Trident/');
     if (trident > 0) {
         // IE 11 => return version number
-        var rv = ua.indexOf('rv:');
-        var version =  parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-        console.log('IE 11');
+        const rv = ua.indexOf('rv:');
+        const version =  parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        console.log('IE 11', version);
         res.sendFile(__dirname + '/public/views/unknown_device.html');
 
         return res.set('Permissions-Policy', 'interest-cohort=()'); // Opt-out of Google FLoC
@@ -96,9 +135,9 @@ app.get('/*', browserDetect, function (req, res) {
     res.set('Permissions-Policy', 'interest-cohort=()'); // Opt-out of Google FLoC
 });
 
-var host = process.env.HOST || null;
+const host = process.env.HOST || null;
 
-var portHttp = process.env.PORT || 3000;
+const portHttp = process.env.PORT || 3000;
 http.createServer(app).listen(portHttp, host, function (err, res) {
     if (err) {
         console.log('Failed to start HTTP server on port' + portHttp, err);
@@ -109,8 +148,8 @@ http.createServer(app).listen(portHttp, host, function (err, res) {
 
 
 if (app.get('env') === 'development') {
-    var portHttps = process.env.PORT_SSL || 3001;
-    var options = {
+    const portHttps = process.env.PORT_SSL || 3001;
+    const options = {
         key: fs.readFileSync('./config/certs/dev.citizenos.com.key'),
         cert: fs.readFileSync('./config/certs/dev.citizenos.com.crt')
     };
