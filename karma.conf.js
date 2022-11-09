b99a8bf7a799e08cfc06d25b3bcd727fc8d3731a
ilmar <ilmar.tyrk@gmail.com>
2022-11-09 13:17:02 +0200

    Add karma testing for accessibility

---
+++ karma.conf.js
@@ -0,0 +1,73 @@
+// Karma configuration
+// Generated on Tue Nov 08 2022 15:32:35 GMT+0200 (Eastern European Standard Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
+    frameworks: ['jasmine', 'aChecker'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        'test/baseline/**/*.json',
+        'public/views/**/*.html',
+        'test/**/*.js'
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+        'test/l10n.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
+    preprocessors: {
+        'public//views/**/*.html': ['html2js'],
+        'test/baseline/**/*.json': ['aChecker'],
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+    reporters: ['progress', 'aChecker'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser instances should be started simultaneously
+    concurrency: Infinity
+  })
+}
