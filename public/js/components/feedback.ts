1ee9e6d999329493142e1748bac99650f1684166
ilmar <ilmar.tyrk@gmail.com>
2023-01-25 19:26:05 +0200

    https://github.com/citizenos/citizenos-fe/issues/1074

--- public/js/components/feedback.ts
+++ public/js/components/feedback.ts
@@ -5,32 +5,38 @@ let feedback = {
     templateUrl: '/views/components/feedback.html',
     bindings: {},
     controller: ['$http', 'sLocation', '$translate', 'sNotification', class FeedbackController {
-        public message;
-        public message2;
+        public name = '';
+        public email = '';
+        public org = '';
+        public message = '';
+        public allowContact = false;
         public error = false;
-        public error2 = false;
         public isSubmitted = false;
         constructor (private $http, private sLocation, private $translate, private sNotification) {
 
         }
 
         submitFeedback () {
-            this.error = this.error2 = false;
+            this.error = false;
             const q1 = this.$translate.instant('MODALS.GIVE_FEEDBACK_QUESTION_1');
             const q2 = this.$translate.instant('MODALS.GIVE_FEEDBACK_QUESTION_2');
             const path = this.sLocation.getAbsoluteUrlApi('/api/internal/feedback');
             if (!this.message ) {
                 this.error = true;
             }
-            if(!this.message2) {
-                this.error2 = true;
+            if (this.error) return;
+            let allowContact = 'No';
+            if(this.allowContact) {
+                allowContact = 'Yes';
             }
-            if (this.error || this.error2) return;
             const finalMessage = `
+            Name: ${this.name}
+            Email: ${this.email}
+            Group/Organization: ${this.org}
             ${q1}
             ${this.message}
             ${q2}
-            ${this.message2}
+            ${allowContact}
             `;
             return this.$http.post(path, {message:finalMessage}).then(() => {
                 this.isSubmitted = true;
